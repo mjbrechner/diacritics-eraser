51c9094e79cda69cb0ea72bb3251b85af982ffae
MJ Brechner <mjbrechner@gmail.com>
2022-09-05 18:51:44 -0400

    added copy function & preliminary diacritics work

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// Erase diacritics and other special characters
+
+function eraseDiacritics() {
+    let e = document.getElementById("textbox").value;
+    document.getElementById("textbox").value = e.replace(/[áăắặằẳẵǎâấậầẩẫäạàảāąåǻãɑ]/g, 'a');
+    // document.getElementById("textbox").value = e.replace(/[æǽ]/g, 'ae');
+}
+
+// Copy text to clipboard
 function copyToClipboard() {
-    alert("Text copied to clipboard");
+    let e = document.getElementById("textbox").value
+    if (e !== ``) {
+        navigator.clipboard.writeText(e);
+        alert("Text copied to clipboard");
+    } else {
+        alert("no text to copy");
+    }
 }
